257751d8ae3ddc94c72b249cbb160e121286c951
Nadya-Clariessa <nclriessa@gmail.com>
2024-09-25 09:59:56 +0700

    Update input.py

--- input.py
+++ input.py
@@ -2,5 +2,11 @@ import streamlit as st
 
 x = st.number_input("Masukkan suhu")
 sx = st.text_input("Satuan c")
-st.write("Amda menginput suhu ", x," dengan satuan ", sx )
+st.write("Anda menginput suhu ", x," dengan satuan ", sx )
 st.write("Kuadratnya ", x*x)
+if (sx == "c"):
+  pass
+else:
+  pass
+
+st.write("Hasil konversi ", x," dengan satuan ", x,sx," adalah ...", sy )
